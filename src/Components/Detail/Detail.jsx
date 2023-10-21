037d3ead864c69323a0987d076ae598f1c4e2c6d
Gayathri <rasangikahw@gmail.com>
2023-10-21 11:23:27 +0530

    modifie

--- src/Components/Detail/Detail.jsx
+++ src/Components/Detail/Detail.jsx
@@ -15,7 +15,7 @@ export const Detail = () => {
                 <div className="search-bar">
                     <input
                         type="text"
-                        placeholder="City,Latitude and longitude"
+                        placeholder="City, Latitude and Longitude"
                         value={searchQuery}
                         className='Sinput'
                         onChange={(e) => setSearchQuery(e.target.value)}
@@ -23,20 +23,76 @@ export const Detail = () => {
                     <button onClick={handleSearch} className='search'>Search</button>
                 </div>
                 <div className="box-container">
-                    <div className="box"><p>Today</p></div>
-                    <div className="box"><p>Tommarrow</p></div>
-                    <div className="box"><p>Day After Tommarrow</p></div>
+                    <div className="box">
+                        <h1>Today</h1>
+                        <form className="form">
+                            <div className="form-group">
+                                <label>Temperature</label>
+                                <input type="text" placeholder="Temperature" />
+                            </div>
+                            <div className="form-group">
+                                <label>Humidity</label>
+                                <input type="text" placeholder="Humidity" />
+                            </div>
+                            <div className="form-group">
+                                <label>Wind</label>
+                                <input type="text" placeholder="Wind" />
+                            </div>
+                            <div className="form-group">
+                                <label>Status</label>
+                                <input type="text" placeholder="Status" />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="box">
+                        <h1>Tomorrow</h1>
+                        <form className="form">
+                            <div className="form-group">
+                                <label>Temperature</label>
+                                <input type="text" placeholder="Temperature" />
+                            </div>
+                            <div className="form-group">
+                                <label>Humidity</label>
+                                <input type="text" placeholder="Humidity" />
+                            </div>
+                            <div className="form-group">
+                                <label>Wind</label>
+                                <input type="text" placeholder="Wind" />
+                            </div>
+                            <div className="form-group">
+                                <label>Status</label>
+                                <input type="text" placeholder="Status" />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="box">
+                        <h1>Day After Tomorrow</h1>
+                        <form className="form">
+                            <div className="form-group">
+                                <label>Temperature</label>
+                                <input type="text" placeholder="Temperature" />
+                            </div>
+                            <div className="form-group">
+                                <label>Humidity</label>
+                                <input type="text" placeholder="Humidity" />
+                            </div>
+                            <div className="form-group">
+                                <label>Wind</label>
+                                <input type="text" placeholder="Wind" />
+                            </div>
+                            <div className="form-group">
+                                <label>Status</label>
+                                <input type="text" placeholder="Status" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
-
                 <div>
-                <Link to="/moredetails">
-                  <button className='seem'>See more</button>
-                </Link>
-                  
+                    <Link to="/moredetails">
+                        <button className='seem'>See more</button>
+                    </Link>
                 </div>
             </div>
         </div>
     );
 };
-
-  
